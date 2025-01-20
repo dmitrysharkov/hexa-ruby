91455839cf5c12428d09f6b60ccf1fb28954c4a5
Dmitry Sharkov <dmitry.sharkov@gmail.com>
2025-01-20 21:21:44 +0100

    added coercing to Null

--- lib/hexa/values/null.rb
+++ lib/hexa/values/null.rb
@@ -6,6 +6,14 @@ module Hexa
       include ScalarMixin
 
       self.base_class = NilClass
+
+      coerce String do |val|
+        if val == 'null' || val.empty?
+          [nil, true]
+        else
+          [nil, false]
+        end
+      end
     end
   end
 end
