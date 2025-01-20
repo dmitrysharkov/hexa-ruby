108da3672bca58c5f00630c6738602c71edd7232
Dmitry Sharkov <dmitry.sharkov@gmail.com>
2025-01-20 23:39:34 +0100

    refactored json writer

--- lib/hexa/json.rb
+++ lib/hexa/json.rb
@@ -2,8 +2,9 @@ module Hexa
   module Json
     def self.generate(val, **options)
       io = StringIO.new
-      stream = WriteStream.new(io, **options)
+      stream = Writer.new(io, **options)
       stream.write(val)
+      io.close
       io.string
     end
   end
--- lib/hexa/json/writer.rb
+++ lib/hexa/json/writer.rb
@@ -2,7 +2,7 @@
 
 module Hexa
   module Json
-    class WriteStream
+    class Writer
       def initialize(io_stream, **options)
         @io_stream = io_stream
         @options = options
@@ -64,56 +64,6 @@ module Hexa
       def out(*args)
         @io_stream.write(*args)
       end
-
-      # def start_object(_record)
-      #   write('{')
-      # end
-      #
-      # def end_object(_record)
-      #   write('}')
-      # end
-      #
-      # def start_object_attribute(_record, attr, _value, _index, _is_last)
-      #   write(attr.camelized_name_str, ':')
-      # end
-      #
-      # def end_object_attribute(_record, _attr, _value, _index, is_last)
-      #   write(',') unless is_last
-      # end
-      #
-      # def start_array(_arr_type, _arr)
-      #   write('[')
-      # end
-      #
-      # def end_array(_arr_type, _arr)
-      #   write(']')
-      # end
-      #
-      # def start_array_item(_arr_type, _arr, _value, _index, _is_last); end
-      #
-      # def end_array_item(_arr_type, _arr, _value, _index, is_last)
-      #   write(',') unless is_last
-      # end
-      #
-      # def write_int(value)
-      #   write(value.to_s)
-      # end
-      #
-      # def write_str(value)
-      #   write('"', value.gsub('\\', '\\\\').gsub('"', '\\"'), '"')
-      # end
-      #
-      # def write_real(value)
-      #   write(value.to_s)
-      # end
-      #
-      # def write_bool(value)
-      #   write(value ? 'true' : false)
-      # end
-      #
-      # def write_null
-      #   write('null')
-      # end
     end
   end
 end
